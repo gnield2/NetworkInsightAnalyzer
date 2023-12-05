df01f40a3e10b73026da133ddf9c7dcf88357729
gnield2 <gnield2@nd.edu>
2023-12-05 10:17:01 -0500

    Added Comments, improved readability

--- plot_data.py
+++ plot_data.py
@@ -24,6 +24,8 @@ def display_stats(data):
     print('Quartiles:               ' + str(statistics.quantiles(data)))
 
 def main():
+    # The user must specify an argument for the particular folder/date they want to read from
+    # i.e. $> python3 plot_data.py 2023-10-02 to read data from October 02, 2023
     directory = 'data/' + sys.argv[1]
     json_files = [f for f in os.listdir(directory) if f.endswith('.json')]
     bssids = []
@@ -31,28 +33,38 @@ def main():
     signals = []
 
     for file_name in json_files:
+        # Use the name of each json file to pinpoint the time getDeviceData.py was run
         time_str = os.path.splitext(file_name)[0]
         hours, minutes = map(int, time_str.split('_'))
         decimal_time = hours + (minutes / 60.0)
         timestamps.append(decimal_time)
 
+        # read the data from each json file
         file_path = os.path.join(directory, file_name)
         data = read_json_file(file_path)
 
+        # Divide the data into two different arrays: BSSIDs and Signals
+        # You can change this if you want to read different data, but this is the data
+        # that was most pertinent for this project.
         current_connection = data.get("Current Connection", {})
         bssid = current_connection.get("BSSID")
         signal = current_connection.get("Signal")
-
         bssids.append(bssid)
         signals.append(signal)
 
+    # Print out the mean, median, std. dev, and quartiles for signal strength
     display_stats(signals)
 
+    # Enumerate the BSSIDs based on how they were read
+    # i.e., the first distinct BSSID is labeled as 1, second as 2, etc.
     bssid_to_index = {bssid: index for index, bssid in enumerate(bssids)}
 
+    # plot the data for the BSSIDs with respect to time
     y_values = [bssid_to_index[bssid] for bssid in bssids]
     plot_data(timestamps, y_values, "Nieuwland 184")
     
+    # this is so that the x-axis shows the time at which getDeviceData.py was run
+    # it doesn't work perfectly--some values show up twice on the axis
     hour_labels = [f'{int(hour)}:{math.floor(int((hour % 1) * 60)):02}' for hour in timestamps]
     plt.xticks(timestamps, hour_labels)
     plt.show()
--- plot_speedData.py
+++ plot_speedData.py
@@ -25,6 +25,8 @@ def display_stats(data, data_name):
     print('Quartiles:               ' + str(statistics.quantiles(data)))
 
 def main():
+    # The user must specify an argument for the particular folder/date they want to read from
+    # i.e. "$> python3 plot_data.py 2023-10-02" to read data from October 02, 2023
     directory = 'speedData/' + sys.argv[1]
     json_files = [f for f in os.listdir(directory) if f.endswith('.json')]
     downloadSpeeds = []
@@ -33,30 +35,38 @@ def main():
     timestamps = []
 
     for file_name in json_files:
+        # Use the name of each json file to pinpoint the time test_speed.py was run
         time_str = os.path.splitext(file_name)[0]
         hours, minutes = map(int, time_str.split('_'))
         decimal_time = hours + (minutes / 60.0)
         timestamps.append(decimal_time)
 
+        # read the data from each json file
         file_path = os.path.join(directory, file_name)
         data = read_json_file(file_path)
-
+        
+        # Divide the data into three different arrays: Down Speed, Up Speed, and Ping
+        # You can change this if you want to read different data, but this is the data
+        # that was most pertinent for this project.
         ds = data.get("Download Speed (Mbps)")
         us = data.get("Upload Speed (Mbps)")
         p = data.get("Ping (ms)")
-
         downloadSpeeds.append(ds)
         uploadSpeeds.append(us)
         pings.append(p)
 
+    # Print out the mean, median, std. dev, and quartiles for all parameters
     display_stats(downloadSpeeds, "Download Speed (Mbps)")
     display_stats(uploadSpeeds, "Upload Speed (Mbps)")
     display_stats(pings, "Ping (ms)")
 
+    # plot the data for each parameter with respect to time
     plot_data(timestamps, downloadSpeeds, "Download Speed (Mbps)", "Download Speed (Mbps)")
     plot_data(timestamps, uploadSpeeds, "Upload Speed (Mbps)", "Upload Speed (Mbps)")
     plot_data(timestamps, pings, "Ping (ms)", "Ping (ms)")
 
+    # this is so that the x-axis shows the time at which getDeviceData.py was run
+    # it doesn't work perfectly--some values show up twice on the axis
     hour_labels = [f'{int(hour)}:{math.floor(int((hour % 1) * 60)):02}' for hour in timestamps]
     plt.xticks(timestamps, hour_labels)
     plt.show()
