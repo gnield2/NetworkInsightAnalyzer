74660a0c2e492bd21294cf27ec80abaa8154f3ee
gnield2 <gnield2@nd.edu>
2023-12-08 21:42:43 -0500

    setup script for Windows

---
+++ setup.bat
@@ -0,0 +1,23 @@
+@echo off
+
+REM Check if Python3 is installed
+where python >nul 2>&1
+
+if errorlevel 1 (
+    echo Python 3 is not installed. Please install Python 3.
+    exit /b 1
+)
+
+REM Check if pip3 is installed
+where pip >nul 2>&1
+if errorlevel 1 (
+    echo pip3 is not installed. Installing pip3...
+    REM You might need to set up the PATH variable to include Python's Scripts directory
+    REM (e.g., set PATH=%PATH%;C:\PythonXX\Scripts)
+    REM Then use 'python -m ensurepip' to install pip if it's not available
+    python -m ensurepip
+)
+
+REM Install necessary Python libraries
+echo Installing necessary python libraries...
+pip3 install subprocess matplotlib.pyplot schedule speedtest
